3df0bda282afedb64b7ecac4e69abc0be60502f7
AlexTolstikhin <alexeytolstikhin@gmail.com>
2020-02-04 22:11:35 -0800

    cs

--- fetchJson/index.ts
+++ fetchJson/index.ts
@@ -2,5 +2,13 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then(response => {
-  console.log(response.data);
+    const {
+        data: { id, title, completed }
+    } = response;
+
+    console.log(`
+        The Todo with id: ${id}
+        Has a title of: ${title}
+        Is it completed? ${completed}
+    `);
 });
