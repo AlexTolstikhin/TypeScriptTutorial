ee3ae8e53e924dcf0ca52f9cd9d86c917ff4d821
AlexTolstikhin <alexeytolstikhin@gmail.com>
2020-02-05 21:18:24 -0800

    Annotaions and Inference

--- fetchJson/index.ts
+++ fetchJson/index.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 axios.get(url).then(response => {
-    const {
-        data: { id, title, completed }
-    } = response;
+    const todo = response.data as Todo;
 
     console.log(`
-        The Todo with id: ${id}
-        Has a title of: ${title}
-        Is it completed? ${completed}
+        The Todo with id: ${todo.id}
+        Has a title of: ${todo.title}
+        Is it completed? ${todo.completed}
     `);
 });
