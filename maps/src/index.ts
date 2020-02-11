8ebed835bc998770d76560ec311bdeb2dafba9f9
AlexTolstikhin <alexeytolstikhin@gmail.com>
2020-02-10 20:40:36 -0800

    added google maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there');
+// import { User } from './User';
+// import { Company } from './Company';
+
+const map = new google.maps.Map(document.getElementById('googlemap'), {
+    zoom: 3,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
